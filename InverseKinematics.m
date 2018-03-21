e2a6f541c328199cf60a37e13004c72eb7ef788a
BrunoAlfredo <36961934+BrunoAlfredo@users.noreply.github.com>
2018-03-21 11:43:19 +0000

    Correction of DirectKinematics
    Change of the distance in the T05 matrix in RoboticaGarcia. Correction of the x,y,z formulas in the DirectKinematics.  Start of InverseKinematics

--- InverseKinematics.m
+++ InverseKinematics.m
@@ -1,6 +1,5 @@
 function theta = InverseKinematics(alpha,beta,gama,x,y,z)
 
-theta = zeros(1,6);
 % Getting the convention matrix
 firstLine = [cos(alpha)*cos(gama) - sin(alpha)*cos(beta)*sin(gama) ...
     -cos(alpha)*sin(gama) - sin(alpha)*cos(beta)*cos(gama) ...
@@ -13,12 +12,26 @@ fourthLine = [0 0 0 1];
 conventionMatrixT06 = [firstLine;secondLine;thirdLine;fourthLine];
 
 % Getting theta1
-theta(1) = atan2(conventionMatrixT06(2,1),conventionMatrixT06(1,1));
+theta = pi/2 + atan2(conventionMatrixT06(2,1),conventionMatrixT06(1,1));
+theta = [theta; theta + pi];
 
 % Getting theta2
 posJoint5toFrame6 = [0;0;-20e-3];
 transformation = conventionMatrixT06*[posJoint5toFrame6;1];
 posJoint5toFrame0 = transformation(1:3);
 
+%caso em que não ha triangulo
+
+%distance for theta 3: 120mm
+
+% Getting theta3
+
+%caso em que não ha triangulo
+
+% Getting theta4
+
+% Getting theta5
+
+% Getting theta6
 end
 
--- RoboticaGarcia.m
+++ RoboticaGarcia.m
@@ -10,16 +10,38 @@ T23 = [cos(theta(3)) -sin(theta(3)) 0 L(3);sin(theta(3)) cos(theta(3)) 0 0;...
          0 0 1 0; 0 0 0 1];
 T34 = [cos(theta(4)) -sin(theta(4)) 0 L(4);0 0 1 0;...
          -sin(theta(4)) -cos(theta(4)) 0 0; 0 0 0 1];
-     
-T45 = [0 1 0 L(7); -1 0 0 0; 0 0 1 0; 0 0 0 1];
+        
+T45 = [0 1 0 0; -1 0 0 0; 0 0 1 L(7); 0 0 0 1];
 
 T56 = [cos(theta(6)) -sin(theta(6)) 0 0; cos(theta(5))*sin(theta(6)) cos(theta(5))*cos(theta(6)) -sin(theta(5)) -sin(theta(5))*20e-3;...
          sin(theta(6))*sin(theta(5)) cos(theta(6))*sin(theta(5)) cos(theta(5)) cos(theta(5))*20e-3; 0 0 0 1];
 
-T01(theta) = T01;
+%T01(theta) = T01;
 T02(theta) = simplify(T01*T12);
 T03(theta) = simplify(T02*T23);
 T04(theta) = simplify(T03*T34);
 T05(theta) = simplify(T04*T45);
 T06(theta) = simplify(T05*T56)
 
+T06(0,0,0,0,0,0);
+
+%% Inverse Kinematics
+%orientation = sym('orientation',[1 3]);
+%position = sym('position',[1 3]);
+% baseT = [cos(orientation(1))*cos(orientation(3))-sin(orientation(1))*cos(orientation(2))*sin(orientation(3))...
+%     -cos(orientation(1))*sin(orientation(3))-sin(orientation(1))*cos(orientation(2))*cos(orientation(3)) ...
+%     sin(orientation(1))*sin(orientation(2)) position(1);...
+%     sin(orientation(1))*cos(orientation(3))+cos(orientation(1))*cos(orientation(2))*sin(orientation(3))...
+%     -sin(orientation(1))*sin(orientation(3))+cos(orientation(1))*cos(orientation(2))*cos(orientation(3))...
+%     -cos(orientation(1))*sin(orientation(2)) position(2);...
+%     sin(orientation(2))*sin(orientation(3)) sin(orientation(2))*cos(orientation(3)) ...
+%     cos(orientation(2)) position(3); 0 0 0 1];
+T13(theta) = simplify(T12*T23);
+T14(theta) = simplify(T13*T34);
+T15(theta) = simplify(T14*T45);
+T16(theta) = simplify(T15*T56);
+
+T35(theta) = simplify(T34*T45);
+T36(theta) = simplify(T35*T56);
+
+%T45(theta) = simplify();
